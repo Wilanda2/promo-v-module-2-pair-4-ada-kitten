522ac61f5f07ee2300c8614c74eb69f05ba7561f
LauraSabrin <waila_05@hotmail.com>
2023-12-21 11:39:31 +0100

    Values

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 let catList = document.querySelector(".js-list");
 
 const kittenOneImg = "https://dev.adalab.es/gato-siames.webp";
-const kittenOneTitle = "Anastasio";
+const kittenOneTitle = "Anastasio".toUpperCase();
 const kittenOneRace = "Siamés";
 const kittenOneDescription = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
 
@@ -17,11 +17,18 @@ const kittenTwoDescription = "Produce fascinación y curiosidad. Exótico, raro,
 const kittenThreeImg = "https://dev.adalab.es/maine-coon-cat.webp";
 const kittenThreeTitle = "Cielo";
 const kittenThreeRace = "Maine Coon";
-const kittenThreeDescrption = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenThreeDescription = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
 
-// const kittenName = document.querySelector('.uppercase');
+const btnAdd = document.querySelector('.js-btn-add');
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const labelMessageError = document.querySelector('.js-label-error');
 
-
+const valueDesc = inputDesc.value;
+const valuePhoto = inputPhoto.value;
+const valueName = inputName.value;
+// const valueRace = inputRace.value;
 
 const kittenOne = `<li class="card">
 <article>
@@ -55,7 +62,7 @@ const kittenThree = `<li class="card">
 />
 <h3 class="card_title">${kittenThreeTitle}</h3>
 <h4 class="card_race">${kittenThreeRace}</h4>
-<p class="card_description">${kittenThreeDescrption}</p>
+<p class="card_description">${kittenThreeDescription}</p>
 </li>`
 
 
@@ -64,5 +71,13 @@ catList.innerHTML = `<li>${kittenOne} </li>`;
 catList.innerHTML += `<li>${kittenTwo}</li>`;
 catList.innerHTML += `<li>${kittenThree}</li>`;
 
+btnAdd.addEventListener('click',()=>{
+  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+    labelMessageError.innerHTML = "¡Uy!Parece que has olvidado algo.";
+  } else {
 
-kittenOneTitle.classList.add('uppercase');
+    
+  }
+});
+
+//¿Hemos de añadir los value a los elementos HTML para poder completar arriba la condición en caso de no cumplimentarse los campos?
